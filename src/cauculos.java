f62d11dfe7fedeb95d8ec313e4fc1e4175f78a15
Zep213 <jososephbem100@gmail.com>
2024-08-13 14:38:09 -0300

    questao_9

---
+++ src/cauculos.java
@@ -0,0 +1,14 @@
+public class cauculos {
+    public static void main (String agrs[]) {
+        byte num1 = 2;
+        byte num2 = 4;
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+        int divisao = num1 / num2;
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
