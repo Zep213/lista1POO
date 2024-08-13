b9d060370d9d1ad03ac7cd04bc376b566077e3a2
Zep213 <jososephbem100@gmail.com>
2024-08-13 14:20:31 -0300

    2_questoes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String agrs[]){
         System.out.println("ola mundo");
+        byte num1 = 5;
+        byte num2 = 6;
+        System.out.println(num1 + num2);
+
+
     }
 }
