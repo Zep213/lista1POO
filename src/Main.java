643e79ab3b8c5b3590ade6e4e373f9df8c4275e6
Zep213 <jososephbem100@gmail.com>
2024-08-13 14:30:45 -0300

    8_questoes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String agrs[]){
-        System.out.println("ola mundo");
+        System.out.println("olá mundo!!");
         byte num1 = 5;
         byte num2 = 6;
         System.out.println(num1 + num2);
+        double num3 = 5.67;
+        System.out.println(num3 * 3);
+        char let1 = 'a';
+        System.out.println(let1);
+        boolean tempc = true;
+        String nome = "joseph";
+        System.out.printf("%s eh um aluno dedicado\n",nome);
+        boolean temCarteira = true;
+        String ola = "olá ";
+        String mundo = "mundo!!";
+        System.out.print(ola + mundo);
+
 
 
     }
